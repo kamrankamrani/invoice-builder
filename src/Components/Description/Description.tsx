d211966880452dbe2a04d5e869b7d1d45ed78715
kamrankamrani <kamrankamrani.fard@yahoo.com>
2023-02-04 17:23:52 +0330

    feat: add description fix: description size and responsive with clamp

--- src/Components/Description/Description.tsx
+++ src/Components/Description/Description.tsx
@@ -1,5 +1,35 @@
+import { useState } from "react";
+import { EnglishNumber, PersianNumber } from "../../Services/ConvertNumbers";
 import "./Style/style.css";
 
 export function Description() {
-  return <h1> I am description </h1>;
+  const [invoiceNum, setInvoiceNum] = useState("0");
+  const date = new Intl.DateTimeFormat("fa-IR").format(new Date());
+  const handleInvoiceNumber = (val: string) => {
+    const _val = Number(EnglishNumber(val));
+    if (isNaN(_val)) return;
+    setInvoiceNum(String(_val));
+  };
+  return (
+    <div className="desc-container">
+      <div className="title">
+        <div className="grow">
+          <p>فاکتور فروش</p>
+        </div>
+        <div className="date">
+          <div className="flex">
+            <p>شماره: </p>
+            <input
+              value={PersianNumber(invoiceNum)}
+              onChange={(e) => handleInvoiceNumber(e.target.value)}
+            />
+          </div>
+          <div className="flex">
+            <p>تاریخ:</p>
+            <p>{`${date}`}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
